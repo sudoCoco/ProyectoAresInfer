604c3a9059f2366b8641ed9e8610dbf3f9aa247d
Raquel Gopar <raquelbgcz@gmail.com>
2021-06-24 15:28:14 +0200

    Agregar archivos de proyecto.

---
+++ Agencia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoAresInfer
+{
+    public class Agencia
+    {
+        public int AgenciaId { get; set; }
+        public int CodigoAgencia { get; set; }
+    }
+}
